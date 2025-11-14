b818bb2c8307b67a4f1ea8a334cbfb9218561be9
Brandon Alfaro <brandonalfaro20@gmail.com>
2025-11-14 00:02:09 -0600

    added mock table rows from json files and populated all tables

--- salon_app/models.py
+++ salon_app/models.py
@@ -1,23 +1,28 @@
 from django.db import models
 
 class Customer(models.Model):
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15)
     date_joined = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
+
 
 
 class Employee(models.Model):
-    name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
     specialty = models.CharField(max_length=100)
     phone = models.CharField(max_length=15)
     email = models.EmailField(unique=True)
+    availability_status = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.name
+        return f"{self.first_name} {self.last_name}"
+
 
 
 class Service(models.Model):
