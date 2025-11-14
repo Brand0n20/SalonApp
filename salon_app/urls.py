b31680a362047cb5dff8e35f51f1222a2a99f0b6
Brandon Alfaro <brandonalfaro20@gmail.com>
2025-11-13 22:04:07 -0600

    created html pages for all templates

--- salon_app/urls.py
+++ salon_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('services/', views.services_list, name='services_list'),
+    path('book/', views.book_appointment, name='book_appointment'),
+    path('customer/<int:customer_id>/', views.customer_profile, name='customer_profile'),
 ]
